aff679c8af70a8093d83c640aab7c3ce78c55396
Terry Lin <yuxlin21@gmail.com>
2022-11-12 13:56:04 -0500

    add students

---
+++ lab7/includes/addStudent.php
@@ -0,0 +1,20 @@
+<?php include "connect.php"; ?>
+<?php
+  $sql = "INSERT INTO students (RIN, RCSID, first-name, last-name, alias, phone, state, city, street, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ssssssssss", $RIN, $RCSID, $firstname, $lastname, $alias, $phone, $state, $city, $street, $zip);
+  $RIN = $_POST['RIN'];
+  $RCSID = $_POST['RCSID'];
+  $firstname = $_POST['first-name'];
+  $lastname = $_POST['last-name'];
+  $alias = $_POST['alias'];
+  $phone = $_POST['phone'];
+  $state = $_POST['state'];
+  $city = $_POST['city'];
+  $street = $_POST['street'];
+  $zip = $_POST['zip'];
+  $stmt->execute();
+  $stmt->close();
+  $conn->close();
+  header("Location: ../index.php");
+?>
--- lab7/students.php
+++ lab7/students.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Grade Book</title>
+  <last-name>Grade Book</last-name>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <style>
@@ -28,12 +28,12 @@
                 </div>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li class="nav-item active">
-                        <a href="#" class="nav-link px-0 current">
+                        <a href="#" class="nav-link px-0">
                         <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Courses</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link px-0">
+                        <a href="#" class="nav-link px-0  current">
                         <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Students</span> </a>
                     </li>
                     <li class="nav-item">
@@ -62,7 +62,7 @@
         <div class="col py-3 text-center">
             <div class="row">
                 <div class="col-12">
-                  <h1>Courses</h1>
+                  <h1>Students</h1>
                 </div>
               </div>
               <div class="row">
@@ -70,17 +70,21 @@
                   <table class="table table-striped">
                     <thead>
                       <tr>
-                        <th scope="col">CRN</th>
-                        <th scope="col">prefix</th>
-                        <th scope="col">number</th>
-                        <th scope="col">title</th>
-                        <th scope="col">section</th>
-                        <th scope="col">year</th>
+                        <th scope="col">RIN</th>
+                        <th scope="col">RCSID</th>
+                        <th scope="col">first-name</th>
+                        <th scope="col">last-name</th>
+                        <th scope="col">alias</th>
+                        <th scope="col">phone</th>
+                        <th scope="col">state</th>
+                        <th scope="col">city</th>
+                        <th scope="col">street</th>
+                        <th scope="col">zip</th>
                       </tr>
                     </thead>
                     <tbody>
                       <?php
-                        $query = "SELECT * FROM courses";
+                        $query = "SELECT * FROM students";
                         $result = $conn->query($query);
                         if (!$result) {
                           echo "SELECT failed: $query<br>" . $conn->error . "<br><br>";
@@ -90,12 +94,16 @@
                             $result->data_seek($j);
                             $row = $result->fetch_array(MYSQLI_ASSOC);
                             echo "<tr>";
-                            echo "<td>" . $row['CRN'] . "</td>";
-                            echo "<td>" . $row['prefix'] . "</td>";
-                            echo "<td>" . $row['number'] . "</td>";
-                            echo "<td>" . $row['title'] . "</td>";
-                            echo "<td>" . $row['section'] . "</td>";
-                            echo "<td>" . $row['year'] . "</td>";
+                            echo "<td>" . $row['RIN'] . "</td>";
+                            echo "<td>" . $row['RCSID'] . "</td>";
+                            echo "<td>" . $row['first-name'] . "</td>";
+                            echo "<td>" . $row['last-name'] . "</td>";
+                            echo "<td>" . $row['alias'] . "</td>";
+                            echo "<td>" . $row['phone'] . "</td>";
+                            echo "<td>" . $row['state'] . "</td>";
+                            echo "<td>" . $row['city'] . "</td>";
+                            echo "<td>" . $row['street'] . "</td>";
+                            echo "<td>" . $row['zip'] . "</td>";
                             echo "</tr>";
                           }
                         }
@@ -106,40 +114,55 @@
           </div>
           <div class="row">
             <div class="col-12">
-              <h2>Add a course</h2>
+              <h2>Add a student</h2>
             </div>
-            <form action="includes/addCourse.php" method="post">
+            <form action="includes/addStudents.php" method="post">
               <div class="row">
                 <div class="col-2">
-                  <label for="CRN">CRN</label>
-                  <input type="text" class="form-control" name="CRN" id="CRN">
+                  <label for="RIN">RIN</label>
+                  <input type="text" class="form-control" name="RIN" id="RIN">
                 </div>
                 <div class="col-2">
-                  <label for="prefix">prefix</label>
-                  <input type="text" class="form-control" name="prefix" id="prefix">
+                  <label for="RCSID">RCSID</label>
+                  <input type="text" class="form-control" name="RCSID" id="RCSID">
                 </div>
                 <div class="col-2">
-                  <label for="number">number</label>
-                  <input type="text" class="form-control" name="number" id="number">
+                  <label for="first-name">first-name</label>
+                  <input type="text" class="form-control" name="first-name" id="first-name">
                 </div>
                 <div class="col-2">
-                  <label for="title">title</label>
-                  <input type="text" class="form-control" name="title" id="title">
+                  <label for="last-name">last-name</label>
+                  <input type="text" class="form-control" name="last-name" id="last-name">
                 </div>
                 <div class="col-2">
-                  <label for="section">section</label>
-                  <input type="text" class="form-control" name="section" id="section">
+                  <label for="alias">alias</label>
+                  <input type="text" class="form-control" name="alias" id="alias">
                 </div>
                 <div class="col-2">
-                  <label for="year">year</label>
-                  <input type="text" class="form-control" name="year" id="year">
+                  <label for="phone">phone</label>
+                  <input type="text" class="form-control" name="phone" id="phone">
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-12">
-                  <button type="submit" class="btn btn-primary">Add</button>
+                <div class="col-2">
+                  <label for="state">state</label>
+                  <input type="text" class="form-control" name="state" id="state">
                 </div>
-              </div>
+                <div class="col-2">
+                  <label for="city">city</label>
+                  <input type="text" class="form-control" name="city" id="city">
+                </div>
+                <div class="col-2">
+                  <label for="street">street</label>
+                  <input type="text" class="form-control" name="street" id="street">
+                </div>
+                <div class="col-2">
+                  <label for="zip">zip</label>
+                  <input type="text" class="form-control" name="zip" id="zip">
+                </div>
+                <div class="row">
+                  <div class="col-12">
+                    <button type="submit" class="btn btn-primary">Add</button>
+                  </div>
+                </div>
             </form>
         </div>
 </body>
