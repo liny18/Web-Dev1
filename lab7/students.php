60cde5f859f4e9c6d584bfcd8112a36bef79fd9d
Terry Lin <yuxlin21@gmail.com>
2022-11-12 19:34:52 -0500

    test

--- lab7/students.php
+++ lab7/students.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Grade Book</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 <style>
     .nav-item a:hover{
@@ -90,7 +91,7 @@
                 </form>
                 <br>
                 <form name="filtering" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                  <button type="submit" name="filter" class="btn btn-dark m-2">> 90</button>
+                  <button type="submit" name="filter" class="btn btn-dark m-2">Nerds</button>
                 </form>
                 <tbody>
                     <?php
