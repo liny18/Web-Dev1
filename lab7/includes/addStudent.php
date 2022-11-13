d584766a4490bd28d00ab8ace722434793261000
Terry Lin <yuxlin21@gmail.com>
2022-11-12 20:17:54 -0500

    Update addStudent.php

--- lab7/includes/addStudent.php
+++ lab7/includes/addStudent.php
@@ -1,6 +1,6 @@
 <?php include "connect.php"; ?>
 <?php
-  $sql = "INSERT INTO students (RIN, RCSID, 'first-name', 'last-name', alias, phone, state, city, street, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  $sql = "INSERT INTO students (RIN, RCSID, `first-name`, `last-name`, alias, phone, state, city, street, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("ssssssssss", $RIN, $RCSID, $firstname, $lastname, $alias, $phone, $state, $city, $street, $zip);
   $RIN = $_POST['RIN'];
