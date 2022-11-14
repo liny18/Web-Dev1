f7b2bf1b608d56d24761a6a8b97620c0627b6f99
Terry Lin <yuxlin21@gmail.com>
2022-11-13 21:04:24 -0500

    test

---
+++ lab8/resources/script.js
@@ -0,0 +1,57 @@
+const allLabs = document.querySelector('#labs');
+const allLectures = document.querySelector('#lectures');
+const allPicks = document.querySelectorAll('.picks');
+const cardImage = document.querySelector('.card-img-top');
+const cardBody = document.querySelector('.card-body');
+const refreshButton = document.querySelector('#refresh');
+
+window.onload = loadcontent();
+
+function loadcontent() {
+fetch('resources/websys.json')
+  .then(response => response.json())
+  .then(data => {
+    const labs = data.Websys_course.Labs;
+    const lectures = data.Websys_course.Lectures;
+    labs.forEach(lab => {
+      const labItem = document.createElement('li');
+      labItem.classList.add('dropdown-item', 'picks');
+      labItem.innerText = lab.Title;
+      labItem.setAttribute("onclick", `show('${lab.Title}', '${lab.Description}', '${lab.Image}', '${lab.Link}')`);
+      allLabs.appendChild(labItem);
+    });
+    lectures.forEach(lecture => {
+      const lectureItem = document.createElement('li');
+      lectureItem.classList.add('dropdown-item', 'picks');
+      lectureItem.innerText = lecture.Title;
+      lectureItem.setAttribute("onclick", `show('${lecture.Title}', '${lecture.Description}', '${lecture.Image}', '${lecture.Link}')`);
+      allLectures.appendChild(lectureItem);
+    });
+    show(labs[0].Title, labs[0].Description, labs[0].Image, labs[0].Link);
+  });
+}
+
+function show(title, description, image, link) {
+  cardBody.innerHTML = '';
+  const titleEl = document.createElement('h4');
+  titleEl.classList.add('card-title');
+  titleEl.innerText = title;
+  cardBody.appendChild(titleEl);
+  const descriptionEl = document.createElement('p');
+  descriptionEl.classList.add('card-text');
+  descriptionEl.innerText = description;
+  cardBody.appendChild(descriptionEl);
+  const linkEl = document.createElement('a');
+  linkEl.classList.add('btn', 'btn-primary');
+  linkEl.innerText = 'Check it out';
+  linkEl.href = link;
+  cardBody.appendChild(linkEl);
+  cardImage.src = `https://source.unsplash.com/500x300/?${description}`;
+}
+
+refreshButton.addEventListener('click', refresh);
+
+function refresh() {
+  loadcontent();
+  window.location.reload();
+}
