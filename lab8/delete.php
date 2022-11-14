c84dd6da183dc8700547162fe8586476767530a2
Terry Lin <yuxlin21@gmail.com>
2022-11-14 01:30:12 -0500

    test

--- lab8/delete.php
+++ lab8/delete.php
@@ -1,6 +1,6 @@
 <?php include "connect.php"; ?>
 <?php
-if (isset($_POST['detete'])) {
+if (isset($_POST['delete'])) {
   $sql = "DELETE FROM content WHERE Title = ?";
   $stmt = $conn->prepare($sql);
   $stmt->bind_param("s", $title);
