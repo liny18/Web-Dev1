e263e2a6709f03faec82e85d435fcb1fa7d8758c
Terry Lin <yuxlin21@gmail.com>
2022-11-14 02:18:56 -0500

    done baby
    restyling pages

--- lab8/index.php
+++ lab8/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once "connect.php";
 include_once("./CAS-1.4.0/CAS.php");
-phpCAS::client(CAS_VERSION_2_0,'cas.auth.rpi.edu',443,'/cas');
+phpCAS::client(CAS_VERSION_2_0, 'cas.auth.rpi.edu', 443, '/cas');
 
 // This is not recommended in the real world!
 // But we don't have the apparatus to install our own certs...
@@ -9,6 +9,7 @@ phpCAS::setNoCasServerValidation();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,52 +26,54 @@ phpCAS::setNoCasServerValidation();
       background-repeat: no-repeat;
       background-attachment: fixed;
     }
+
     .picks:hover {
       cursor: pointer;
     }
   </style>
 </head>
+
 <body>
   <div class="container-fluid">
     <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div class="d-flex flex-column align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <div class="w-100 text-center border-bottom">
-                  <a href="#" class="pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-1 text-center">Content</span>
-                  </a> 
-                </div>
-                <nav class="navbar nav-tabs nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Labs
-                      </a>
-                      <ul class="dropdown-menu" id="labs">
-                      </ul>
-                    </li>
+      <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+        <div class="d-flex flex-column align-items-sm-start px-3 pt-2 text-white min-vh-100">
+          <div class="w-100 text-center border-bottom">
+            <a href="#" class="pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+              <span class="fs-1 text-center">Content</span>
+            </a>
+          </div>
+          <nav class="navbar nav-tabs nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Labs
+                </a>
+                <ul class="dropdown-menu" id="labs">
+                </ul>
+              </li>
 
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Lectures
-                      </a>
-                      <ul class="dropdown-menu" id="lectures">
-                      </ul>
-                    </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Lectures
+                </a>
+                <ul class="dropdown-menu" id="lectures">
+                </ul>
+              </li>
 
-                    <li class="nav-item">
-                      <a class="nav-link text-white" href="table.php" role="button">
-                        Archives
-                      </a>
-                  </ul>
-                </nav>
-                <hr>
-                <?php 
-                  if (!phpCAS::isAuthenticated()) {
-                    header("Location: out.php");
-                  } else {
-                    echo "User: " . phpCAS::getUser();
-                    echo '
+              <li class="nav-item">
+                <a class="nav-link text-white" href="table.php" role="button">
+                  Archives
+                </a>
+            </ul>
+          </nav>
+          <hr>
+          <?php
+          if (!phpCAS::isAuthenticated()) {
+            header("Location: out.php");
+          } else {
+            echo "User: " . phpCAS::getUser();
+            echo '
                     <div class="dropdown pb-4 border-top">
                           <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                               <img src="https://github.com/liny18.png" alt="user" width="30" height="30" class="rounded-circle">
@@ -85,48 +88,49 @@ phpCAS::setNoCasServerValidation();
                               <li class="ms-3"><a href="logout.php" class="btn btn-primary">Logout</a></li>
                           </ul>
                       </div>';
-                    } 
-                ?>
-            </div>
+          }
+          ?>
         </div>
+      </div>
 
-        <div class="col py-3 align-content-center text-center right">
-            <h2>Pick from the navigation bar on the left</h2>
-            <div class="btn btn-warning" id="refresh">
-              Refresh
-            </div>
-            <div class="row justify-content-center align-items-center">
-              <div class="col-6">       
-                  <div class="card p-2 mt-5">
-                    <img class="card-img-top" src="resources/images/monkey.jpg" alt="Title">
-                    <div class="card-body">
+      <div class="col py-3 align-content-center text-center right">
+        <h2>Pick from the navigation bar on the left</h2>
+        <div class="btn btn-warning" id="refresh">
+          Refresh
+        </div>
+        <div class="row justify-content-center align-items-center">
+          <div class="col-6">
+            <div class="card p-2 mt-5">
+              <img class="card-img-top" src="resources/images/monkey.jpg" alt="Title">
+              <div class="card-body">
+              </div>
+              <div class="card-footer">
+                <form method="post">
+                  <div class="row justify-content-center">
+                    <div class="col-2 d-none">
+                      <input type="text" class="form-control" name="Title" id="Title">
                     </div>
-                    <div class="card-footer">
-                    <form method="post">
-              <div class="row justify-content-center">
-                <div class="col-2 d-none">
-                  <input type="text" class="form-control" name="Title" id="Title">
-                </div>
-                <div class="col-2 d-none">
-                  <input type="text" class="form-control" name="Description" id="Description">
-                </div>
-                <div class="col-2 d-none">
-                  <input type="text" class="form-control" name="Link" id="Link">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-12">
-                  <button type="submit" class="btn btn-success" name="archive" formaction="archive.php">Archive</button>
-                  <button type="submit" class="btn btn-danger" name="delete" formaction="delete.php">Delete</button>
-                </div>
-              </div>
-            </form>
+                    <div class="col-2 d-none">
+                      <input type="text" class="form-control" name="Description" id="Description">
+                    </div>
+                    <div class="col-2 d-none">
+                      <input type="text" class="form-control" name="Link" id="Link">
                     </div>
                   </div>
+                  <div class="row">
+                    <div class="col-12">
+                      <button type="submit" class="btn btn-success" name="archive" formaction="archive.php">Archive</button>
+                      <button type="submit" class="btn btn-danger" name="delete" formaction="delete.php">Delete</button>
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
-    </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </body>
+
 </html>
