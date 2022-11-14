06f9cc434ab359ab86ef17aa873f33e8306a38ca
Terry Lin <yuxlin21@gmail.com>
2022-11-14 02:20:30 -0500

    done baby
    bug fix

--- lab8/table.php
+++ lab8/table.php
@@ -21,7 +21,6 @@ phpCAS::setNoCasServerValidation();
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
     crossorigin="anonymous"></script>
-  <script defer src="resources/script.js"></script>
   <style>
     .right {
       background-image: url("resources/images/monkey.jpg");
