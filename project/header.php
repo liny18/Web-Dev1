a005be3da149b24a48178403f59aba57d124a136
Terry Lin <yuxlin21@gmail.com>
2022-12-01 23:06:05 -0500

    Update header.php

--- project/header.php
+++ project/header.php
@@ -20,7 +20,6 @@
         box-shadow: 0px 0px 10px 1px rgba(0, 0, 0, 0.3);
         background-color: #f04320;
         padding: 5px;
-        margin: 5px;
     }
 
     .navbar .navbar-nav {
@@ -50,7 +49,7 @@
     }
 </style>
 <nav class="navbar navbar-expand-lg navbar-light sticky-top">
-    <div class="container-fluid">
+    <div class="container-fluid d-flex align-items-center">
         <div class="col-3">
             <a class="navbar-brand" href="../main/main.php">
                 <img class="nav-logo" src="../pictures/logo.png" alt="Error Cats logo" width="40" height="40" style="border-radius: 50%;">
