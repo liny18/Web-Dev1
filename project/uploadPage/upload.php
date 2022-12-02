90feaf6ee0dde3fba21784d7af63b31c4facad0c
Terry Lin <yuxlin21@gmail.com>
2022-12-01 23:28:35 -0500

    Update upload.php

--- project/uploadPage/upload.php
+++ project/uploadPage/upload.php
@@ -216,11 +216,6 @@
 
     <br>
 
-    <!-- ADD FOOTER -->
-    <footer>
-        <?php include '../footer.html'; ?>
-    </footer>
-
     <main>
         <form id="uploadPost" class="container" action="upload.php" method="post" enctype="multipart/form-data">
             <div class="row">
@@ -276,6 +271,11 @@
             </div>
         </form>
     </main>
+
+    <!-- ADD FOOTER -->
+    <footer>
+        <?php include '../footer.html'; ?>
+    </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
