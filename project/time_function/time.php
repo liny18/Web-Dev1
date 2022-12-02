eefe5085bb4035163355bdece53d5b3caf6387b0
Terry Lin <yuxlin21@gmail.com>
2022-12-02 02:04:14 -0500

    Update time.php

--- project/time_function/time.php
+++ project/time_function/time.php
@@ -1,49 +1,32 @@
 <?php
-
-
-function calculate_time($date1){
+function calculate_time($date1) {
     //get current time with the format y-m-d h:i:s with time zone America/New_York
     date_default_timezone_set('America/New_York');
     $date2 = (date("Y-m-d H:i:s"));
-
-//get the difference between the two time
+    //get the difference between the two time
     $diff = abs(strtotime($date2) - strtotime($date1));
-
-// if the difference is less than 1 hour, we will show the minutes
-    if ($diff < 3600) {
+    $unit = "second";
+    if ($diff >= 31536000) {
+        $diff = round($diff / 31536000);
+        $diff > 1 ? $unit = "years" : $unit = "year";
+    } else if ($diff >= 2592000) {
+        $diff = round($diff / 2592000);
+        $diff > 1 ? $unit = "months" : $unit = "month";
+    } else if ($diff >= 604800) {
+        $diff = round($diff / 604800);
+        $diff > 1 ? $unit = "weeks" : $unit = "week";
+    } else if ($diff >= 86400) {
+        $diff = round($diff / 86400);
+        $diff > 1 ? $unit = "days" : $unit = "day";
+    } else if ($diff >= 3600) {
+        $diff = round($diff / 3600);
+        $diff > 1 ? $unit = "hours" : $unit = "hour";
+    } else if ($diff >= 60) {
         $diff = round($diff / 60);
-        return $diff . " minutes ago";
+        $diff > 1 ? $unit = "minutes" : $unit = "minute";
     } else {
-        // if the difference is less than 24 hour, we will show the hours
-        if ($diff < 86400) {
-            $diff = round($diff / 3600);
-            return $diff . " hours ago";
-        } else {
-            // if the difference is less than 7 days, we will show the days
-            if ($diff < 604800) {
-                $diff = round($diff / 86400);
-                return $diff . " days ago";
-            } else {
-                // if the difference is less than 30 days, we will show the weeks
-                if ($diff < 2592000) {
-                    $diff = round($diff / 604800);
-                    return $diff . " weeks ago";
-                } else {
-                    // if the difference is less than 365 days, we will show the months
-                    if ($diff < 31536000) {
-                        $diff = round($diff / 2592000);
-                        return $diff . " months ago";
-                    } else {
-                        // if the difference is more than 365 days, we will show the years
-                        $diff = round($diff / 31536000);
-                        return $diff . " years ago";
-                    }
-                }
-            }
-        }
+        $diff > 1 ? $unit = "seconds" : $unit = "second";
     }
+    return $diff . " " . $unit . " ago";
 }
-
-
-
 ?>
