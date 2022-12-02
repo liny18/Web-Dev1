1292f4a910f3a9ae30ab041641e025b45d27f488
Terry Lin <yuxlin21@gmail.com>
2022-12-01 22:52:39 -0500

    Update header.php

--- project/header.php
+++ project/header.php
@@ -50,7 +50,7 @@
 </style>
 <nav class="navbar navbar-expand-lg navbar-light sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand col-3" href="../main/main.php">
+        <a class="navbar-brand" href="../main/main.php">
             <img class="nav-logo" src="../pictures/logo.png" alt="Error Cats logo" width="40" height="40" style="border-radius: 50%;">
             RPI Foodies
         </a>
@@ -67,7 +67,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse justify-content-end col-3" id="navbarScroll">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarScroll">
             <ul class="navbar-nav">
                 <li class="nav-item trending">
                     <a href="../Dining Hall Page/index.php" class="navbar-brand" title="Dinning Halls">
