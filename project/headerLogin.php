1a3461d0c24476092fbffabc137e489362566260
Terry Lin <yuxlin21@gmail.com>
2022-12-01 20:09:19 -0500

    Update headerLogin.php

--- project/headerLogin.php
+++ project/headerLogin.php
@@ -55,44 +55,44 @@
     <body>
         <nav class="navbar navbar-expand-lg navbar-light sticky-top">
             <div class="container-fluid">
-            <a class="navbar-brand" href="../main/main.php">
-                <img src="../pictures/logo.png" alt="Error Cats logo" width="40" height="40" style="border-radius: 50%;">
-                RPI Foodies
-            </a>
-            <form class="d-flex" method="post" action="../header.php">
-                <div class="row">
-                    <div class="col">
-                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+                <a class="navbar-brand" href="../main/main.php">
+                    <img src="../pictures/logo.png" alt="Error Cats logo" width="40" height="40" style="border-radius: 50%;">
+                    RPI Foodies
+                </a>
+                <form class="d-flex" method="post" action="../header.php">
+                    <div class="row">
+                        <div class="col">
+                            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+                        </div>
+                        <div class="col">
+                            <button class="btn btn-outline-light" type="submit" name="submitSearch" id="submitSearch" value="submitSearch" title="Search">
+                                <img src="../pictures/search_ideogram.svg" alt="Magnifying glass" width="25" height="25">
+                            </button>
+                        </div>
                     </div>
-                    <div class="col">
-                        <button class="btn btn-outline-light" type="submit" name="submitSearch" id="submitSearch" value="submitSearch" title="Search">
-                            <img src="../pictures/search_ideogram.svg" alt="Magnifying glass" width="25" height="25">
-                        </button>
-                    </div>
+                </form>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarScroll">
+                    <ul class="navbar-nav">
+                        <li class="nav-item trending">
+                            <a href="../Dining Hall Page/index.php" class="navbar-brand" title="Dinning Halls">
+                                <img src="../pictures/trendingIcon.svg" alt="trending button" width="40" height="40">
+                            </a>
+                        </li>
+                        <li class="nav-item post">
+                            <a href="../uploadPage/upload.php" class="navbar-brand" title="Upload">
+                                <img src="../pictures/addPostIcon.svg" alt="add post button" width="40" height="40">
+                            </a>
+                        </li>
+                        <li class="nav-item logOut">
+                            <a href="../phpcas/logout.php" class="navbar-brand">
+                                <button id="logOut" class="btn btn-outline-light" type="submit">Log Out</button>
+                            </a>
+                        </li>
+                    </ul>
                 </div>
-            </form>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarScroll">
-                <ul class="navbar-nav">
-                <li class="nav-item trending">
-                    <a href="../Dining Hall Page/index.php" class="navbar-brand" title="Dinning Halls">
-                        <img src="../pictures/trendingIcon.svg" alt="trending button" width="40" height="40">
-                    </a>
-                </li>
-                <li class="nav-item post">
-                    <a href="../uploadPage/upload.php" class="navbar-brand" title="Upload">
-                        <img src="../pictures/addPostIcon.svg" alt="add post button" width="40" height="40">
-                    </a>
-                </li>
-                <li class="nav-item logOut">
-                    <a href="../phpcas/logout.php" class="navbar-brand">
-                        <button id="logOut" class="btn btn-outline-light" type="submit">Log Out</button>
-                    </a>
-                </li>
-                </ul>
-            </div>
             </div>
         </nav>
     </body>
